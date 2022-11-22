0a13e342ef93db067f9de787ebafaa417427a3e1
Edgar Leung <edsDbug@gmail.com>
2022-11-22 09:32:55 -0500

    Add profile pic and new new designs

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,21 +3,20 @@ import './styles/home.scss';
 
 export default function Home() {
   return (
-      <div className="home card" style={{maxWidth: "540px"}}>
-        <div className="row g-0">
-         
+    <div className="home d-flex justify-content-center align-items-center">
+      <div className="home__block card">
+        <div className="row g-0">  
           <div className="col-md-8">
             <div className="card-body">
               <h1 className="card-title">EDGAR LEUNG</h1>
               <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
             </div>
           </div>
           <div className="col-md-4">
-            <img src="..." className="img-fluid rounded-start" alt="..."/>
+            <img src={require("../Pictures/ME.jpg")} className="img-fluid rounded-circle" alt="..."/>
           </div>
         </div>
       </div>
-    
+    </div>
   )
 }
