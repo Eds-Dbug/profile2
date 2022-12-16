8f17b768f4d05b84879261f93c7dcbfd34f5ecbb
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-12-15 20:44:56 -0500

    add conditional for copied to clipboard and toggle useEffect.

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -4,25 +4,41 @@ import { contacts } from '../data/Data';
 import { IconPrefix } from '@fortawesome/fontawesome-svg-core';
 import { IconName } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
 
 export default function Contact() {
 
-	let navigate = useNavigate();
+	const [copyToBoard,setCopyToBoard ] = useState(false); 
+
 	const routeChange = (pathUrl: string) => {
 		window.open(pathUrl);
 	};
 
 	const copyToClipBoard = (text: string) => {
 		navigator.clipboard.writeText(text);
+		//need to set new state to true
+		setCopyToBoard(true);
 	}
+
+	useEffect(() => {
+		setTimeout(()=> {
+			setCopyToBoard(false);
+		},3000)
+	}, [copyToBoard]);
 
   const contactMap = contacts.map(contact => {
     return (
-			<div className="card contact__cards__card">
-				<div className="body">
+			<>
+				<div className="card contact__cards__card">
+					{!contact.link && copyToBoard ? <h6>Copied to Clipboard</h6> : <></>}
+					<div className="body">
 						<a
 							className="btn btn-outline-success"
-							onClick={contact.link? () => routeChange(contact.link) : () => copyToClipBoard(contact.platform)}
+							onClick={
+								contact.link
+									? () => routeChange(contact.link)
+									: () => copyToClipBoard(contact.platform)
+							}
 						>
 							<FontAwesomeIcon
 								className="icon"
@@ -33,9 +49,9 @@ export default function Contact() {
 							/>
 							<h5>{contact.platform}</h5>
 						</a>
-					
+					</div>
 				</div>
-			</div>
+			</>
 		);
   });
 
