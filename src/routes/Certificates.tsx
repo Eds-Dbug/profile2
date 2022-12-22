54b226bc067a46cc399cd394d050d3b712f56de0
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-12-22 17:01:50 -0500

    certifications opens tab to new links

--- src/routes/Certificates.tsx
+++ src/routes/Certificates.tsx
@@ -4,12 +4,20 @@ import { certificates } from "../data/Data";
 
 export default function Certificates() {
 	const certificateMap = certificates.map((cert) => {
-		return <div className="card certificates__grid__item">
-			<img src={cert.pic} className="card-img-top" alt="..."/>
-			<div className="card-body">
-				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		return (
+			<div className="card certifications__grid__item">
+				<img
+					src={cert.pic}
+					className="btn btn-outline-success card-img-top"
+					alt="..."
+					onClick={() => window.open(cert.link)}
+				/>
+	
+				<div className="card-body">
+					<p className="card-text">{cert.description}</p>
+				</div>
 			</div>
-		</div>;
+		);
 	}) ;
 
   return (
