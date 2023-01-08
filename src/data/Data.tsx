6207b2364490b09db1be2b63a8a64125f352ab53
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2023-01-08 17:06:33 -0500

    import new project data and photos

--- src/data/Data.tsx
+++ src/data/Data.tsx
@@ -195,4 +195,72 @@ const softwareSkills = [
 	},
 ];
 
+const projectData = [
+	{
+		id: 'scheduler_app',
+		pic: In_Progress,
+		link: 'https://github.com/Eds-Dbug/scheduler',
+		description:
+			'An app that allows for the creation of meetings between interviewers and students',
+	},
+	{
+		id: 'Videos_Search_App',
+		pic: Videos_Search_App,
+		link: 'https://eds-dbug.github.io/videos/',
+		description:
+			'App that searches for videos, that was created during Modern React with Redux Course',
+	},
+	{
+		id: 'Seasons_App',
+		pic: Seasons_React_App,
+		link: 'https://eds-dbug.github.io/Determine_season_app/',
+		description:
+			'App that Checks if its chilly or sunny outside depending on your location',
+	},
+	{
+		id: 'Image_Search_App',
+		pic: Image_Search_App,
+		link: 'https://eds-dbug.github.io/Image_Search_App/',
+		description:
+			'App that searches for images depending on whatever the user puts in the search bar',
+	},
+	{
+		id: 'Student_List_App',
+		pic: Student_List_App,
+		link: 'https://eds-dbug.github.io/front_end_assessment-2/',
+		description:
+			'An app that renders a list of students and can search for a student by name or by tags that can be added. For more information please contact me.',
+	},
+	{
+		id: 'Technical_Documentation_Page',
+		pic: Technical_Documentation_Page,
+		link: 'https://codepen.io/TeeJip/full/zYEWKgy',
+		description: 'A technical documentation page for dear old JavaScript',
+	},
+	{
+		id: 'Tribute_Page',
+		pic: Tribute_Page,
+		link: 'https://codepen.io/TeeJip/full/gOGogpq',
+		description:
+			'A tributary page for Dr. Norman Junior a project from the Free Code Camp',
+	},
+	{
+		id: 'Random_Quote_Machine',
+		pic: Random_Quote_Machine,
+		link: 'https://codepen.io/TeeJip/full/qBmvwWX',
+		description: 'A Quote machine that generates random quotes',
+	},
+	{
+		id: 'Survey_Form',
+		pic: Survey_Form,
+		link: 'https://codepen.io/TeeJip/full/zYEpZMQ',
+		description: 'My first survey form',
+	},
+	{
+		id: 'In Progress',
+		pic: In_Progress,
+		description: 'Work in Progress',
+	},
+];
+
 export { contacts, links, hobbyLinks, certificates, softwareSkills };
