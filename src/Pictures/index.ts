f202ef2589cf19e2f5312184b754678a3f013ba6
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2023-01-08 17:30:59 -0500

    Add new images to index.

--- src/Pictures/index.ts
+++ src/Pictures/index.ts
@@ -1,5 +1,17 @@
 import JS_AlGO_Cert from './JS Algorithms and Data Structures.png';
 import RESPONSIVE_WEB_DESIGN from './Responsive Web Design Cert.png';
 import ME from './ME.jpg';
+import BAY from './BAY.jpg';
+import IMAGE_SEARCH_APP from './Image_Search_App.png';
+import IN_PROGRESS from './In_Progress.jpg';
+import NIAGRA from './Niagra.jpg';
+import EDDIES_MUSIC from "./Personal_Portfolio_Eddie's_Music.png"
+import RANDOM_QUOTE_MACHINE from './Random_Quote_Machine.png';
+import SEASONS_APP from './Seasons_React_App.png';
+import STUDENT_LIST from './Student_List_App.png';
+import TECH_DOCS from './Technical_Documentation.png';
+import TRIBUTE from './Tribute_Page_Test.png';
+import VIDEO_SEARCH_APP from './Videos_Search_App.png';
+
 
 export { JS_AlGO_Cert, RESPONSIVE_WEB_DESIGN, ME };
