f21011d8d15465408d5f54ff3b174a35246f26c7
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2023-01-10 22:30:03 -0500

    Update projects and Data to be passed into projects.

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -4,11 +4,25 @@ import { NavLink } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 
 export default function Projects() {
+	{
+		/**grid for mapping */
+	}
+	const arrayChunk = (arr: number[], n: number) => {
+		const array = arr.slice();
+		const chunks = [];
+		while (array.length) chunks.push(array.splice(0, n));
+		return chunks;
+	}
+
   return (
 		<div>
-			fsfdsf Projects
-			<NavLink to='cycling'>Cycling</NavLink>
-			<Outlet />
+			{arrayChunk([...Array(9).keys()], 3).map((row, i) => (
+				<div key={i} className="row mx-auto">
+					{row.map((col, i) => (
+						<span key={i}>[{col}]</span>
+					))}
+				</div>
+			))}
 		</div>
 	);
 }
