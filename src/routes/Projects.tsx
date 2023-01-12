7c8a345d71b0ebd1389eebc7ce041a2d4475a76a
Edgar Leung <edsDbug@gmail.com>
2023-01-12 13:34:13 -0500

    add card design

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -1,9 +1,3 @@
-import React from 'react'
-import { Outlet } from 'react-router';
-import { NavLink } from 'react-router-dom';
-import NavBar from '../NavBar/NavBar';
-import {projectData} from '../data/Data';
-
 export default function Projects() {
 	{
 		/**grid for mapping */
@@ -18,7 +12,6 @@ export default function Projects() {
     name: string,
     surname: string
   }
-
 
   const data = [
     { name: 'a', surname: 'b' }, 
@@ -38,31 +31,25 @@ export default function Projects() {
     //console.log(array)
     const chunks = [];
     while (array.length) {
-      // for (const x of array.splice(0, n)) {
-      //   chunks.push(
-      //     <div className='flex mx-auto'>
-      //       <span>{x.name}</span>
-      //       <span>{x.surname}</span>
-      //     </div>
-      //   )
-
       chunks.push(array.splice(0, n));
       }
-      //chunks.push('|')
-
     return chunks;
   };
 	
 
   return (
-		<div>
+		<div className='projects'>
 			{arrayChunk(data, 3).map((row, i) => (
-				<div key={i} className="flex mx-auto">
+				<div key={i} className="projects__row">
+          |
 					{row.map((col, i ) => (
-            <>
-              <span key={i}>{col.name}</span>
-              <span key={i}>{col.surname}</span>
-            </>
+            <div className="card" key={i}>
+              <img src="..." className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <p className="card-text">{col.name}</p>
+                <p className="card-text">{col.surname}</p>
+              </div>
+            </div>
 					))}
 				</div>
 			))}
