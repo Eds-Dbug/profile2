d607e937c69b92581a99b52b8d29bd28c699d101
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2023-01-12 18:16:09 -0500

    Add title to home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,31 +2,38 @@ import React from 'react'
 import { Outlet } from 'react-router';
 import { NavLink } from 'react-router-dom';
 import '../styles/routes/home.scss';
+import Header from '../partials/Header';
 
 export default function Home() {
   return (
-    <div className="home d-flex justify-content-center align-items-center">
-      <div className="home__block card ">
-        <div className="row g-0 ">  
-          <div className="col-md-8">
-            <div className="card-body">
-              <h1 className="card-title">EDGAR LEUNG</h1>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-
-          </div>
-          <div className="col-md-4 ">
-            <img src={require("../Pictures/ME.jpg")} className="img-fluid rounded-circle" alt="..."/>
-          </div>
-          <div>
-            
-            <Outlet/>
-          </div>
-          
-        </div>
-      </div>
-
-      
-    </div>
-  )
+		<>
+			<Header title="Home" />
+			<div className="home d-flex justify-content-center align-items-center">
+				<div className="home__block card ">
+					<div className="row g-0 ">
+						<div className="col-md-8">
+							<div className="card-body">
+								<h1 className="card-title">EDGAR LEUNG</h1>
+								<p className="card-text">
+									This is a wider card with supporting text below as a natural
+									lead-in to additional content. This content is a little bit
+									longer.
+								</p>
+							</div>
+						</div>
+						<div className="col-md-4 ">
+							<img
+								src={require('../Pictures/ME.jpg')}
+								className="img-fluid rounded-circle"
+								alt="..."
+							/>
+						</div>
+						<div>
+							<Outlet />
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
