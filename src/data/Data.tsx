1f5060ec566fb1400ebc5fea6710af2d9216c03f
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2023-01-17 18:02:10 -0500

    remove <p> tags from CyclingText and update links.

--- src/data/Data.tsx
+++ src/data/Data.tsx
@@ -1,4 +1,4 @@
-import {JS_AlGO_Cert, RESPONSIVE_WEB_DESIGN,BAY, IN_PROGRESS, VIDEO_SEARCH_APP, SEASONS_APP, IMAGE_SEARCH_APP, STUDENT_LIST, TECH_DOCS, TRIBUTE, RANDOM_QUOTE_MACHINE, NIAGRA, GIANT_BIKE} from '../Pictures/index'
+import { JS_AlGO_Cert, RESPONSIVE_WEB_DESIGN,BAY, IN_PROGRESS, VIDEO_SEARCH_APP, SEASONS_APP, IMAGE_SEARCH_APP, STUDENT_LIST, TECH_DOCS, TRIBUTE, RANDOM_QUOTE_MACHINE, NIAGRA, GIANT_BIKE, Bruce_Camp} from '../Pictures/index'
 
 const contacts = [
 	{
@@ -86,7 +86,7 @@ const bikeTripLinks = [
 	{
 		id: 1,
 		name: 'City',
-		url: 'city',
+		url: 'City',
 	},
 	{
 		id: 2,
@@ -300,55 +300,49 @@ const CyclingText = [
 		id: 'Fun',
 		pic: GIANT_BIKE,
 		text: `
-			<p>
-				Since I am writing this in the winter, I am less enthusiastic about
-				cycling currently mainly because the weather does not agree. That being
-				said once the weather gets better I am sure I will be cycling once
-				again. I have been a bicyclist all my life, I am used to biking long
-				distances as I will explain below. Bicycling can be used as a
-				combination of excercise, meditation, sight seeing, and transportation
-				and I rate my experience in that order. It is a good excuse to go
-				outside especially if you have one of those days where you arent doing
-				anything. Overall you get to explore the city on your own terms be on
-				the road, sidewalk, alleyway or even through a nice trail.
-			</p>
-		`
+			Since I am writing this in the winter, I am less enthusiastic about
+			cycling currently mainly because the weather does not agree. That being
+			said once the weather gets better I am sure I will be cycling once
+			again. I have been a bicyclist all my life, I am used to biking long
+			distances as I will explain below. Bicycling can be used as a
+			combination of excercise, meditation, sight seeing, and transportation
+			and I rate my experience in that order. It is a good excuse to go
+			outside especially if you have one of those days where you arent doing
+			anything. Overall you get to explore the city on your own terms be on
+			the road, sidewalk, alleyway or even through a nice trail.
+		`,
 	},
 	{
 		id: 'Niagra',
 		pic: NIAGRA,
-		text: `
-			<p>
-				I have biked to Niagra Falls from Toronto on multiple occasions (5
-				times). The first time around was the most taxing but it was just a
-				matter of willpower moreso than any physical endurance (though it did
-				require some). The trail I took was the Lakes Waterfront Trail as it ran
-				through the city of Toronto all the way to the falls. Biking through
-				Oakville and other communties like Burlington was a treat but the best
-				part is definitely cycling through the country side where it was filled
-				with open fields as far as the eye can see. Lots of good fruits too from
-				the farmers out there. It almost became a ritual, as there was back to
-				back trips every subsequent summer from I believe 2014. Theres a map on
-				the side to show you the route taken.
-			</p>
-		`
+		text: `	
+			I have biked to Niagra Falls from Toronto on multiple occasions (5
+			times). The first time around was the most taxing but it was just a
+			matter of willpower moreso than any physical endurance (though it did
+			require some). The trail I took was the Lakes Waterfront Trail as it ran
+			through the city of Toronto all the way to the falls. Biking through
+			Oakville and other communties like Burlington was a treat but the best
+			part is definitely cycling through the country side where it was filled
+			with open fields as far as the eye can see. Lots of good fruits too from
+			the farmers out there. It almost became a ritual, as there was back to
+			back trips every subsequent summer from I believe 2014. Theres a map on
+			the side to show you the route taken.
+		`,
 	},
 	{
 		id: 'Georgian Bay',
 		pic: BAY,
 		text: `
-			<p>
-				So biking to Georgian Bay was a tale of tedium and tenacity. The overall
-				trip was not entirely pleasant it was filled with episodes of extreme
-				fatigue and biking through extreme heat in the day to extreme cold in
-				the pitch darkness. The route we took is shown on the left but suffice
-				to say it was a grueling journey with many days of unremarkable progress
-				through mostly flat terrain and small towns though at times we did stop
-				by many provincial parks where we busted out our hammock tents to sleep
-				I can speak volumes on the entire trip if you would like to know more I
-				will be making a seperate article/webpage talking more about this.
-			</p>
-		`
+			So biking to Georgian Bay was a tale of tedium and tenacity. The overall
+			trip was not entirely pleasant it was filled with episodes of extreme
+			fatigue and biking through extreme heat in the day to extreme cold in
+			the pitch darkness. The route we took is shown on the left but suffice
+			to say it was a grueling journey with many days of unremarkable progress
+			through mostly flat terrain and small towns though at times we did stop
+			by many provincial parks where we busted out our hammock tents to sleep
+			I can speak volumes on the entire trip if you would like to know more I
+			will be making a seperate article/webpage talking more about this.
+		`,
 	},
 ];
 
