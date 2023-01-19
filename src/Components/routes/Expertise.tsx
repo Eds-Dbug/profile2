587497f85931146087283c53848d1fa810e5146f
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2023-01-19 12:39:04 -0500

    One less todo

--- src/Components/routes/Expertise.tsx
+++ src/Components/routes/Expertise.tsx
@@ -1,7 +1,7 @@
 import Header from '../partials/Header';
 import { certificates } from 'data/Data';
-import './_skills.scss';
-import './_certifications.scss';
+
+import styles from './_expertise.module.scss';
 import { softwareSkills } from 'data/Data';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconName, IconPrefix } from '@fortawesome/fontawesome-svg-core';
@@ -11,7 +11,7 @@ export default function Expertise() {
 
 	const certificateMap = certificates.map((cert) => {
 		return (
-			<div className="card certifications__grid__item" key={cert.id}>
+			<div className={`card ${styles.certifications__grid__item}`} key={cert.id}>
 				<img
 					src={cert.pic}
 					className="btn btn-outline-success card-img-top"
@@ -32,10 +32,10 @@ export default function Expertise() {
 
 		return (
 			<section
-				className="card text-center skills__software__body__item"
+				className={`card text-center ${styles.skills__software__body__item}`}
 				key={skill.id}
 			>
-				<div className="card-body btn skills__software__body__item--body">
+				<div className={`card-body btn ${styles["skills__software__body__item--body"]}`}>
 					<a onClick={() => routeChange(skill.link)}>
 						{skill.iconPrefix ? (
 							<FontAwesomeIcon
@@ -46,13 +46,13 @@ export default function Expertise() {
 								]}
 							/>
 						) : (
-							<h6 className="skills__software__body__item--title">
+							<h6 className={`${styles["skills__software__body__item--title"]}`}>
 								{fullUpperCaseName}
 							</h6>
 						)}
 					</a>
 				</div>
-				<div className="card-footer skills__software__body__item--footer">
+				<div className={`card-footer ${styles["skills__software__body__item--footer"]}`}>
 					{upperCaseSkillName}
 				</div>
 			</section>
@@ -62,14 +62,14 @@ export default function Expertise() {
 	return (
 		<div className="">
 			<Header title="Expertise" />
-			<div className="certifications">
-				<section className="certifications__grid">{certificateMap}</section>
+			<div className={styles.certifications}>
+				<section className={styles.certifications__grid}>{certificateMap}</section>
 			</div>
 
-			<div className="skills">
-				<section className="card skills__software">
+			<div className={styles.skills}>
+				<section className={`card ${styles.skills__software}`}>
 					<h2 className="card-header">Software Skills</h2>
-					<div className="card-body skills__software__body">{skillsMap}</div>
+					<div className={`card-body ${styles.skills__software__body}`}>{skillsMap}</div>
 				</section>
 			</div>
 		</div>
