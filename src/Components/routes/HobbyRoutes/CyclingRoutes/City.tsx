c3c4fd3ac41fc9f8f3a5798e4ac027d290115dcd
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2023-01-23 09:38:25 -0500

    imgs now all same size.

--- src/Components/routes/HobbyRoutes/CyclingRoutes/City.tsx
+++ src/Components/routes/HobbyRoutes/CyclingRoutes/City.tsx
@@ -1,10 +1,11 @@
-import {CyclingText} from 'data/Data'
+import {CyclingText} from 'data/Data';
+import styles from './_Hobbies.module.scss';
 
 export default function City() {
   const cityMap = CyclingText.filter((item) => item.id === 'Fun').map((item,i) => (
-     <div key={i}>
-      <img src={item.pic} alt="Toronto" />
-      <p>{item.text}</p>
+     <div className={styles.place} key={i}>
+      <img className={styles.place__img} src={item.pic} alt="Toronto" />
+      <p className={styles.place__description}>{item.text}</p>
     </div>)
   );
 
